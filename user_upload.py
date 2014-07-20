990a5160736d64ddc0feffbfd38e2c45bd468341
MeherRafei <meher.rafei@gmail.com>
2014-07-20 17:15:15 +1000

    updated file parsing

--- user_upload.py
+++ user_upload.py
@@ -1,27 +1,60 @@
 #!/usr/bin/env python2.7
 
-import sys, getopt
+import sys, getopt, MySQLdb
+
+def stripped(x):
+   return "".join([i for i in x if 31 < ord(i) < 127])
+
 
 def main (argv):
-   inputfile = ''
-   outputfile = ''
+   csv_file = ''
+   users = []
+   sections = ''
+   mysql_user = ''
+   mysql_pass = ''
+   mysql_host = ''
    try:
-      opts, args = getopt.getopt(argv,"hi:o:",["ifile=","ofile="])
+      opts, args = getopt.getopt(argv,"u:p:h:",["help","file=","create_table","dry_run"])
    except getopt.GetoptError:
-      print 'test'
+      print 'user_upload.py --file <CSV file name>'
+      print 'user_upload.py --create_table'
+      print 'user_upload.py --dry_run'
+      print 'user_upload.py -u'
+      print 'user_upload.py -p'
+      print 'user_upload.py -h'
+      print 'user_upload.py --help'
       sys.exit(2)
 
    for opt, arg in opts:
-      if opt == '-h':
-         print 'test.py -i <inputfile> -o <outputfile>'
+      if opt == "--help":
+         print 'user_upload.py --file <CSV file name>'
+         print 'user_upload.py --create_table'
+         print 'user_upload.py --dry_run'
+         print 'user_upload.py -u'
+         print 'user_upload.py -p'
+         print 'user_upload.py -h'
+	 print 'user_upload.py --help'
          sys.exit()
-      elif opt in("-i","--ifile"):
-         inputfile = arg
-      elif opt in("-o", "--ofile"):
-         outputfile = arg
+      elif opt == "--file":
+         csv_file = arg
+         with open(csv_file) as fp:
+            for line in fp:
+               users.append(line.rstrip().strip(" ").split(','))
+            fp.close()
+            print users
+            sections = users[0]
+            print sections
+      elif opt == "--create_table":
+         csv_file = arg
+      elif opt == "--dry_run":
+         csv_file = arg
+      elif opt == "-u":
+         mysql_user = arg
+      elif opt == "-p":
+         mysql_pass = arg
+      elif opt == "-h":
+         mysql_host = arg
 
-   print 'Input file is "', inputfile
-   print 'Input file is "', outputfile
 
 if __name__ == "__main__":
    main(sys.argv[1:])
